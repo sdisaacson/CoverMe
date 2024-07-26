db2654286ace5a56ed2c22d69f6348ba93a11acb
dino1908 <68874367+dino1908@users.noreply.github.com>
2024-07-26 22:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ Best Regards,
 Input 1 - Resume of applying candidate: {context}
 Input 2 - Job description for the job to apply: {input}
 Note: The output should only contain the cover letter as given in the format structure. 
+"""
 
 @st.cache_resource
 def load_embeddings():
